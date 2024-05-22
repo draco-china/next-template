ee477e62b2945886432a9b94e530f97c57d913f3
Draco <draco.coder@gmail.com>
2024-05-23 01:20:05 +0800

    💄 style(page): Update page height

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button';
 export default async function Home({ params: { lng } }: { params: { lng: string } }) {
   const { t } = await getTranslation(lng, 'index');
   return (
-    <div className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <div className='flex min-h-[calc(100vh-65px-85px)] flex-col items-center justify-between p-24'>
       <div className="before:bg-gradient-radial after:bg-gradient-conic relative z-[-1] mt-24 flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
